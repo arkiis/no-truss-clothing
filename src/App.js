283bad2d68c2e19fea0c4d04af325603184c7371
Your Name <william.fleckenstein@gmail.com>
2019-07-23 19:20:35 -0500

    implemented firebase utulities and added a sign-up componenet

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-ou
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop";
 import Header from "./components/header/header";
-import { auth } from "./firebase/firebase";
+import { auth, createUserProfileDocument } from "./firebase/firebase";
 
 class App extends React.Component {
   constructor() {
@@ -22,8 +22,19 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+        });
+      } else this.setState({ currentUser: userAuth });
     });
   }
 
