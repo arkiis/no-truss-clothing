8f8c0b1be30171230ff7199275df46df7330859e
Your Name <william.fleckenstein@gmail.com>
2019-07-26 18:19:51 -0500

    created a category page for each individual section

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,27 +1,14 @@
 import React from "react";
+import { Route } from "react-router-dom";
 
-import SHOP_DATA from "./shop.data";
+import CollectionsOverview from "../../components/collections-overview/collections-overview";
+import CollectionPage from "../category/category";
 
-import CollectionPreview from "../../components/preview-collection/preview-collection";
-
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      collections: SHOP_DATA
-    };
-  }
-
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...otherCollection }) => (
-          <CollectionPreview key={id} {...otherCollection} />
-        ))}
-      </div>
-    );
-  }
-}
+const ShopPage = ({ match }) => (
+  <div className="shop-page">
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+  </div>
+);
 
 export default ShopPage;
