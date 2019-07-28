93950dab450b1f8a37f6635bc4626f81f3da5b42
Your Name <william.fleckenstein@gmail.com>
2019-07-27 23:58:23 -0500

    testing something

--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -24,13 +24,6 @@ const INTIAL_STATE = {
       size: "large",
       id: 4,
       linkUrl: "shop/womens"
-    },
-    {
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-      size: "large",
-      id: 5,
-      linkUrl: "shop/mens"
     }
   ]
 };
